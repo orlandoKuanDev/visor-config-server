6c24dd858af030336b313867cde02c0e83f5562a
Orlando Jose Kuan Becerra <orlandojose.kuanbecerra@everis.nttdata.com>
2021-09-08 18:11:02 -0500

    Initial commit

---
+++ src/main/java/com/example/visorconfigserver/MsVisorConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.visorconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVisorConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVisorConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/visorconfigserver/MsVisorConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visorconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVisorConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
